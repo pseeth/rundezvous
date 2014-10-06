c6078e3625226dd29af146a4e22450a0efdbd505
Prem Seetharaman <prem@u.northwestern.edu>
2014-10-05 21:45:40 -0500

    more stuff

--- static/js/start.js
+++ static/js/start.js
@@ -1,16 +1,38 @@
 phases = ['#home', '#rundetails', '.direction', '#done'];
 step = 0;
 
+directions = ['left', 'right', 'straight']
+dirstep = 0;
+
 function advance() {
 	$(phases[step-1]).hide();
 	$(phases[step]).show();
+	console.log(phases[step]);
+}
+
+function updateDirection() {
+	$('.direction').text(directions[dirstep]);
 }
 
 $(document).ready(function() {
 	$('#next').click(function() {
 		step += 1;
 		if (step < phases.length) {
-			advance();
+			if (phases[step] == '.direction') {
+				if (dirstep == 0) {
+					advance();
+				}
+				updateDirection();
+				dirstep += 1;
+				step -= 1;
+				
+				if (dirstep >= directions.length) {
+					step += 1;
+				}
+			} 
+			else {
+				advance();
+			}
 		}
 	});
 	$('.screen').hide();
